781cfd3aa81bd79a99befba36e2a4387c29b5ba8
Ogbuigbo Chijioke <chijiokejames82@gmail.com>
2024-04-11 23:07:52 +0100

    last commit

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -39,8 +39,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         payment_method_types: ["card"],
         customer_email: email,
         mode: "payment",
-        success_url: "https://localhost:1337/checkout/success",
-        cancel_url: "https://localhost:1337",
+        success_url: "http://localhost:1337/checkout/success",
+        cancel_url: "http://localhost:1337",
         line_items: lineItems,
       });
 
